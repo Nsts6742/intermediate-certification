44bbb9961c3b6eee7270679879488528bdb70e77
Nsts6742 <ryzhiy.2020@list.ru>
2024-03-22 14:23:44 +0300

    Created a free array input for the user.

--- mission/Program.cs
+++ mission/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество элементов в массиве: "); //the user sets the number of items
+int number = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[number];
+for (int i = 0; i < array.Length; i++) //the user enters the elements of the array
+{
+    Console.WriteLine("Введите элемент массива: ");
+    array[i] = Console.ReadLine();
+}
+Console.WriteLine(" ");
+Console.WriteLine("Ваш массив: "); //output of the full array
+Console.Write(array[0]);
+for (int i = 1; i < array.Length; i++)
+{
+    Console.Write(", " + array[i]);
+}
