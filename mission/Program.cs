85bfb0bc613cad86d31ec8feb69b183f9aa08ed1
Nsts6742 <ryzhiy.2020@list.ru>
2024-03-22 14:26:15 +0300

    The solution of the task.

--- mission/Program.cs
+++ mission/Program.cs
@@ -13,3 +13,31 @@ for (int i = 1; i < array.Length; i++)
 {
     Console.Write(", " + array[i]);
 }
+
+string[] arrayOfShortElements = new string[array.Length];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) //search for the right items
+    {
+        arrayOfShortElements[j++] = array[i];
+    }
+    else //in the future: the length of the array with the necessary elements
+    {
+        number--;
+    }
+}
+Console.WriteLine(" ");
+if (number != 0) //output of the required array
+{
+    Console.WriteLine("Элементы вашего массива, длина которых меньше или равна трем: ");
+    Console.Write(arrayOfShortElements[0]);
+    for (int i = 1; i < number; i++)
+    {
+        Console.Write(", " + arrayOfShortElements[i]);
+    }
+}
+else
+{
+    Console.WriteLine("Ваш массив не содержит элементов, необходимых для выполнения программы.");
+}
